a9f2fea19807c10fe5c2cdb0f60ad822ad5af1d6
nikol <nikol@DESKTOP-00DS0F5>
2024-11-14 12:58:41 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Homework7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(56, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Question 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(331, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Question :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(56, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 36);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Answer :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(56, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 36);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "A -";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(56, 320);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 36);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "B -";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(56, 382);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 36);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "C -";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(56, 440);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 36);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "D -";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(139, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(153, 36);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Answer A";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(140, 320);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(152, 36);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Answer B";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(139, 382);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(153, 36);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Answer C";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(139, 440);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(153, 36);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Answer D";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(265, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 49);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(458, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 49);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(648, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 49);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(833, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 49);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1383, 784);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Drivers exam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace Homework7
+{
+    public partial class Form1 : Form
+    {
+        private char[] correctAnswers = new char[]
+        {
+            'A', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D',
+            'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'A'
+        };
+
+        private List<char> studentAnswers;
+        private int questionIndex;
+
+        public Form1()
+        {
+            InitializeComponent();
+            studentAnswers = new List<char>();
+            questionIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading correct answers. Starting test.", "Information");
+
+            if (correctAnswers.Length == 0)
+            {
+                MessageBox.Show("Correct answers are not provided in the program.", "Error");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Correct answers loaded. Loading first question.", "Information");
+                LoadNextQuestion();
+            }
+        }
+
+        private void LoadNextQuestion()
+        {
+            if (questionIndex < correctAnswers.Length)
+            {
+                label1.Text = $"Question {questionIndex + 1}";
+                label2.Text = $"Question text {questionIndex + 1}";
+                label9.Text = "A";
+                label10.Text = "B";
+                label11.Text = "C";
+                label12.Text = "D";
+            }
+            else
+            {
+                GradeExam();
+            }
+        }
+
+        private void GradeExam()
+        {
+            int correctCount = 0;
+            List<int> incorrectQuestions = new List<int>();
+
+            for (int i = 0; i < correctAnswers.Length; i++)
+            {
+                if (i < studentAnswers.Count && studentAnswers[i] == correctAnswers[i])
+                {
+                    correctCount++;
+                }
+                else
+                {
+                    incorrectQuestions.Add(i + 1);
+                }
+            }
+
+            int incorrectCount = correctAnswers.Length - correctCount;
+            string resultMessage = correctCount >= 15 ? "Passed" : "Failed";
+
+            MessageBox.Show($"Result: {resultMessage}\n" +
+                            $"Correct Answers: {correctCount}\n" +
+                            $"Incorrect Answers: {incorrectCount}\n" +
+                            $"Incorrect Questions: {string.Join(", ", incorrectQuestions)}");
+        }
+
+        private void RecordAnswerAndProceed(char answer)
+        {
+            studentAnswers.Add(answer);
+            questionIndex++;
+
+            if (questionIndex < correctAnswers.Length)
+            {
+                LoadNextQuestion();
+            }
+            else
+            {
+                GradeExam();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecordAnswerAndProceed('A');
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RecordAnswerAndProceed('B');
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RecordAnswerAndProceed('C');
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RecordAnswerAndProceed('D');
+        }
+    }
+}
